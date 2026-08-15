9f2daaaebaedf0a4b7fa34b30154b381c09e21ad
ammarlounissi <onismr@gmail.com>
2026-08-15 10:52:43 +0100

    السماح للمتصفحات بتحميل ملف json

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ import models, schemas
 from database import engine, get_db
 from json import JSONDecodeError
 from whatsapp_service import send_whatsapp_message
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Hojrat Bladi API", version="1.0.0")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 # --- Endpoints المصانع ---
 
